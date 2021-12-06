5a53c5fa80164370608c5c8d658cc7e9bd837e29
PaddyHQ <patrickjholmquist@gmail.com>
2021-12-06 10:48:12 -0800

    saving whatever was on machine

--- Practice.py
+++ Practice.py
@@ -1,5 +1,3 @@
-voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, 
-                {"county":"Denver", "registered_voters": 463353}, 
-                {"county":"Jefferson", "registered_voters": 432438}]
-for county_dict in voting_data:
-    print(f"{county_dict['county']} county has {county_dict['registered_voters']:,} registered voters.")
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for key, value in counties_dict.items():
+    print(f"{key} county has {value:,} registered voters.")
