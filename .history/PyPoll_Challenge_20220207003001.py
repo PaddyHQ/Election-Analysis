6c22c415165dae86c35a3f70aace3e50796f7723
Patrick Holmquist <patrickjholmquist@gmail.com>
2022-02-07 00:45:20 -0800

    done

--- .history/PyPoll_Challenge_20220207003001.py
+++ .history/PyPoll_Challenge_20220207003001.py
@@ -100,7 +100,7 @@ with open(file_to_save, "w") as txt_file:
         county_percentage= float(county_total) / float(total_votes)*100
 
          # 6d: Print the county results to the terminal.
-        county_results= (f"{county_name}:{county_percentage:.1f}%:({county_total:,})\n")
+        county_results= (f"{county_name}:{county_percentage:.1f}%:({county_total:,})")
         print(county_results)
          # 6e: Save the county votes to a text file.
         txt_file.write(county_results)
@@ -114,10 +114,10 @@ with open(file_to_save, "w") as txt_file:
        f"\n-------------------------\n"
         f"Largest County Turnout: {winning_county}\n"
         f"-------------------------\n")
-    print(winning_county_results)
+    print(county_results)
 
     # 8: Save the county with the largest turnout to a text file.
-    txt_file.write(winning_county_results)
+    txt_file.write(county_results)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
--- .history/PyPoll_Challenge_20220207003034.py
+++ .history/PyPoll_Challenge_20220207003034.py
@@ -100,7 +100,7 @@ with open(file_to_save, "w") as txt_file:
         county_percentage= float(county_total) / float(total_votes)*100
 
          # 6d: Print the county results to the terminal.
-        county_results= (f"{county_name}:{county_percentage:.1f}%:({county_total:,})\n")
+        county_results= (f"{county_name}:{county_percentage:.1f}%:({county_total:,})")
         print(county_results)
          # 6e: Save the county votes to a text file.
         txt_file.write(county_results)
